a4a6d98840738cdbeb9b6dd03a997b51fda201d3
dmndsword <boris.peso08@gmail.com>
2026-07-25 00:56:55 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
 }
 
 dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.22"))
     implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
 }
