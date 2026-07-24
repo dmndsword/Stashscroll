613a65d1ab547ae073b959e924c382ff81de9c5b
dmndsword <boris.peso08@gmail.com>
2026-07-25 00:42:05 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,31 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.myreels.app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.myreels.app"
+        minSdk = 29
+        targetSdk = 34
+        versionCode = 2
+        versionName = "2.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
 }
